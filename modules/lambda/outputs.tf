90239772005806b3f1b548a94bc929c1381bd6d8
ShenLoong99 <yithuettsk29@gmail.com>
2026-04-14 00:03:30 +0800

    remove unnecessary outputs

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -22,18 +22,3 @@ output "lambda_role_id" {
   description = "The ID of the IAM role used by the Lambda functions"
   value       = aws_iam_role.lambda_role.id
 }
-
-output "sub_logs_arn" {
-  description = "The ARN of the Subscribe Lambda CloudWatch log group"
-  value       = aws_cloudwatch_log_group.sub_logs.arn
-}
-
-output "event_updates_arn" {
-  description = "The ARN of the SNS topic for event updates"
-  value       = aws_sns_topic.event_updates.arn
-}
-
-output "lambda_dlq_arn" {
-  description = "The ARN of the SQS queue used as a dead letter queue for Lambda functions"
-  value       = aws_sqs_queue.lambda_dlq.arn
-}
--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -7,8 +7,3 @@ output "website_url" {
   description = "The URL of the hosted website"
   value       = "http://${aws_s3_bucket_website_configuration.hosting.website_endpoint}"
 }
-
-output "bucket_arn" {
-  description = "The ARN of the S3 bucket used for hosting the website"
-  value       = aws_s3_bucket.website.arn
-}
